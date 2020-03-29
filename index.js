9ebdf6135b29dd067610757a99c85aa3e3ffc46c
Paul Allen <ptallen63@gmail.com>
2020-03-29 19:59:36 -0400

    inital run

--- index.js
+++ index.js
@@ -1,15 +1,67 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const axios = require('axios');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+const baseURL = "https://api.todoist.com/rest/v1";
+
+const getProjectIdFromName = async (name, token) => {
+
+  // Get Projects
+  const response = await axios.get(`${baseURL}/projects`, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+  const project = response.data
+    .filter(p => p.name === name)
+    .pop()
+  return project ? project.id : null;
 }
+
+const createTask = async ({
+  projectId, content, dueString, token
+}) => {
+  const body = {
+    content,
+    due_string: dueString,
+  }
+
+  if (projectId) body.project_id = projectId;
+
+  const res = await axios(`${baseURL}/tasks`,{
+    method: "POST",
+    data: body,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-type": 'application/json'
+    }
+  })
+
+  return res.data;
+}
+
+(async () => {
+
+  try {
+   // Get inputs
+   const taskContent = core.getInput('task-content');
+   const projectName = core.getInput('project-name');
+   const dueString = core.getInput('due-string');
+   const token = core.getInput('token');
+
+   // Get All projects
+    const projectId = await getProjectIdFromName(projectName, token);
+
+    const res = await createTask({
+      content: taskContent,
+      projectId: projectId,
+      dueString: dueString,
+      token,
+    })
+
+    core.setOutput('response-message', res);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})()
